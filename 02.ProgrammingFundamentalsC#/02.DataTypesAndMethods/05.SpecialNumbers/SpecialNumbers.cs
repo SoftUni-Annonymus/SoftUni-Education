0b0629bdf351ef70b32ded9ef0a62b8efdf66148
V-Uzunov <vega_99@abv.bg>
2016-09-04 19:07:28 +0300

    Homeworks And Exercises

---
+++ 02.ProgrammingFundamentalsC#/02.DataTypesAndMethods/05.SpecialNumbers/SpecialNumbers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class SpecialNumbers
+{
+    static void Main()
+    {
+        var n = int.Parse(Console.ReadLine());
+
+        
+        for (int i = 1; i <= n; i++)
+        {
+            var sum = 0;
+            var number = i;
+            while (number>0)
+            {
+                sum += number % 10;
+                number /= 10;
+            }
+            bool ifSumIsEqualOnFive = (sum == 5);
+            bool ifSumIsEqualOnSeven = (sum == 7);
+            bool ifSumIsEqualOnElevan = (sum == 11);
+            bool isSPecial = ifSumIsEqualOnFive || ifSumIsEqualOnSeven || ifSumIsEqualOnElevan;
+            Console.WriteLine("{0} -> {1}", i, isSPecial);
+        }
+    }
+}
+
