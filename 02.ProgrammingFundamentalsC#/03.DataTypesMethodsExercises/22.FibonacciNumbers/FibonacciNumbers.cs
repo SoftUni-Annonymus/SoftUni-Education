a1741379766e3ef2d36f57f0499aa395e36016f0
V-Uzunov <vega_99@abv.bg>
2016-09-12 18:49:00 +0300

    Exercises

---
+++ 02.ProgrammingFundamentalsC#/03.DataTypesMethodsExercises/22.FibonacciNumbers/FibonacciNumbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class FibonacciNumbers
+{
+    static void Main()
+    {
+        var n = int.Parse(Console.ReadLine());
+
+        Fin(n);
+    }
+
+    private static void Fin(int n)
+    {
+        var f1 = 1;
+        var f2 = 0;
+        var nextNumber = 0;
+        for (int i = 0; i <= n; i++)
+        {
+            nextNumber = f1 + f2;
+            f1 = f2;
+            f2 = nextNumber;
+        }
+        Console.WriteLine(f2);
+    }
+}
+
